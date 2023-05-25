3ec58e922936c27bcfff4ec81a2a1c9327d22ac7
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-05-25 11:43:02 +0400

    .

--- src/components/searchCategory/SearchCategory.jsx
+++ src/components/searchCategory/SearchCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import SearchFilter from '../home/SearchFilter'
 import { movieList } from '../data/Data'
